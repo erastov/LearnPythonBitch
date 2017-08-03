f7218a0c41898f4c7b1c0f729784a5b40e940b8d
Erastov Fedor <fedor_erastov@mail.ru>
2017-08-03 14:14:55 +0300

    Add methods last_name and give_raise

--- person.py
+++ person.py
@@ -1,6 +1,19 @@
 class Person(object):
-    def __init__(self, name, job, pay):
+    def __init__(self, name, job=None, pay=0):
         self.name = name
         self.job = job
         self.pay = pay
 
+    def last_name(self):
+        return self.name.split()[-1]
+
+    def give_raise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+
+    print(bob.last_name())
+    sue.give_raise(.20)
+    print(sue)
